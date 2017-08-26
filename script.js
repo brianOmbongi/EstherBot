c0749f50a3803bb0756bc5e6ebfc595f58247f18
brianOmbongi <31278671+brianOmbongi@users.noreply.github.com>
2017-08-26 09:06:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('So you want to learn about  Brian? Just say start to get started.')
                 .then(() => 'speak');
         }
     },
@@ -44,7 +44,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I didn't understand that.`).then(() => 'speak');
+                    return bot.say(`I didn't understand that.You can say start to access something else`).then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
